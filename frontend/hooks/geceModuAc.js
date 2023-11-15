24f6994efcc2a20caa1b555c69f1d3ffa1b1b390
Murat Simsek <muratsimseekk@gmail.com>
2023-11-15 18:14:03 +0300

    reads value on localStorage and changes theme

--- frontend/hooks/geceModuAc.js
+++ frontend/hooks/geceModuAc.js
@@ -2,10 +2,8 @@ import { useState } from "react";
 import useLocalStorageKullan from "./localStorageKullan";
 
 const useGeceModuAc = () => {
-  const [geceModu, setGeceModu] = useLocalStorageKullan(
-    "geceModu",
-    initialValue
-  );
+  const [geceModu, setGeceModu] = useLocalStorageKullan("geceModu", false);
+  // const [geceModu, setGeceModu] = useState(false);
 
   const toggleMode = (e) => {
     e.preventDefault();
